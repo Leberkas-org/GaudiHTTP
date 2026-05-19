eed439f9e6b2403d1bf27fbe6eafdc12c349d49a
st0o0 <64534642+st0o0@users.noreply.github.com>
2026-05-19 20:47:04 +0200

    docs: register HomePage and CodeTabs theme components

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,8 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import LikeC4Diagram from '../components/LikeC4Diagram.vue'
+import HomePage from '../components/HomePage.vue'
+import CodeTabs from '../components/CodeTabs.vue'
 import './custom.css'
 
 export default {
@@ -8,5 +10,7 @@ export default {
     enhanceApp({ app })
     {
         app.component('LikeC4Diagram', LikeC4Diagram)
+        app.component('HomePage', HomePage)
+        app.component('CodeTabs', CodeTabs)
     },
 } satisfies Theme
