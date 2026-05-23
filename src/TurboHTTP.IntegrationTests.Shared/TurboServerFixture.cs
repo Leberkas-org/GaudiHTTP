05c4e49078f1245c049415a0c5902d19bd5f0192
st0o0 <64534642+st0o0@users.noreply.github.com>
2026-05-23 09:40:48 +0200

    feat(tests): add TurboServerFixture for server and E2E integration tests

---
+++ src/TurboHTTP.IntegrationTests.Shared/TurboServerFixture.cs
@@ -0,0 +1,60 @@
+using System.Net;
+using System.Net.Sockets;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Logging;
+using Servus.Akka.Transport;
+using TurboHTTP.Server;
+
+namespace TurboHTTP.IntegrationTests.Shared;
+
+public class TurboServerFixture : IAsyncLifetime
+{
+    private readonly Action<WebApplication> _configureRoutes;
+    private WebApplication? _app;
+
+    public TurboServerFixture(Action<WebApplication> configureRoutes)
+    {
+        _configureRoutes = configureRoutes;
+    }
+
+    public Uri HttpBaseAddress { get; private set; } = null!;
+    public int HttpPort { get; private set; }
+
+    public async ValueTask InitializeAsync()
+    {
+        var port = GetFreePort();
+        var builder = WebApplication.CreateBuilder();
+        builder.Logging.ClearProviders();
+
+        builder.Services.AddTurboKestrel(options =>
+        {
+            options.Bind(new TcpListenerOptions { Host = "127.0.0.1", Port = (ushort)port });
+        });
+
+        _app = builder.Build();
+        _configureRoutes(_app);
+
+        await _app.StartAsync();
+
+        HttpPort = port;
+        HttpBaseAddress = new Uri($"http://127.0.0.1:{port}");
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_app is not null)
+        {
+            await _app.StopAsync();
+            await _app.DisposeAsync();
+        }
+    }
+
+    private static int GetFreePort()
+    {
+        using var listener = new TcpListener(IPAddress.Loopback, 0);
+        listener.Start();
+        var port = ((IPEndPoint)listener.LocalEndpoint).Port;
+        listener.Stop();
+        return port;
+    }
+}
