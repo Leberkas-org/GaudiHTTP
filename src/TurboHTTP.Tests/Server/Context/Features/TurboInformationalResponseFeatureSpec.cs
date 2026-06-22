0806bd0cd74f1501b9b101f48371a69ddf685f1e
st0o0 <64534642+st0o0@users.noreply.github.com>
2026-06-22 19:53:55 +0200

    feat(server): add TurboInformationalResponseFeature for 1xx responses

---
+++ src/TurboHTTP.Tests/Server/Context/Features/TurboInformationalResponseFeatureSpec.cs
@@ -0,0 +1,89 @@
+using Microsoft.AspNetCore.Http;
+using TurboHTTP.Server.Context.Features;
+
+namespace TurboHTTP.Tests.Server.Context.Features;
+
+public sealed class TurboInformationalResponseFeatureSpec
+{
+    [Fact(Timeout = 5000)]
+    public void SendInformational_should_invoke_callback_with_status_and_headers()
+    {
+        int receivedStatus = 0;
+        IHeaderDictionary? receivedHeaders = null;
+        var feature = new TurboInformationalResponseFeature((status, headers) =>
+        {
+            receivedStatus = status;
+            receivedHeaders = headers;
+        });
+
+        var headers = new HeaderDictionary { ["Link"] = "</style.css>; rel=preload" };
+        feature.SendInformational(103, headers);
+
+        Assert.Equal(103, receivedStatus);
+        Assert.Same(headers, receivedHeaders);
+    }
+
+    [Fact(Timeout = 5000)]
+    public void SendInformational_should_accept_100_continue()
+    {
+        var called = false;
+        var feature = new TurboInformationalResponseFeature((_, _) => called = true);
+
+        feature.SendInformational(100, new HeaderDictionary());
+
+        Assert.True(called);
+    }
+
+    [Fact(Timeout = 5000)]
+    public void SendInformational_should_reject_status_below_100()
+    {
+        var feature = new TurboInformationalResponseFeature((_, _) => { });
+
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+            feature.SendInformational(99, new HeaderDictionary()));
+    }
+
+    [Fact(Timeout = 5000)]
+    public void SendInformational_should_reject_status_200_or_above()
+    {
+        var feature = new TurboInformationalResponseFeature((_, _) => { });
+
+        Assert.Throws<ArgumentOutOfRangeException>(() =>
+            feature.SendInformational(200, new HeaderDictionary()));
+    }
+
+    [Fact(Timeout = 5000)]
+    public void SendInformational_should_reject_after_final_response()
+    {
+        var feature = new TurboInformationalResponseFeature((_, _) => { });
+        feature.MarkFinalResponseSent();
+
+        Assert.Throws<InvalidOperationException>(() =>
+            feature.SendInformational(100, new HeaderDictionary()));
+    }
+
+    [Fact(Timeout = 5000)]
+    public void SendInformational_should_allow_multiple_1xx_before_final()
+    {
+        var count = 0;
+        var feature = new TurboInformationalResponseFeature((_, _) => count++);
+
+        feature.SendInformational(100, new HeaderDictionary());
+        feature.SendInformational(103, new HeaderDictionary());
+
+        Assert.Equal(2, count);
+    }
+
+    [Fact(Timeout = 5000)]
+    public void Reset_should_clear_final_response_flag()
+    {
+        var called = false;
+        var feature = new TurboInformationalResponseFeature((_, _) => called = true);
+        feature.MarkFinalResponseSent();
+        feature.Reset();
+
+        feature.SendInformational(100, new HeaderDictionary());
+
+        Assert.True(called);
+    }
+}
