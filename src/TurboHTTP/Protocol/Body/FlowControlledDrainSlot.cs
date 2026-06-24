d88dfb570faac69463ebb2a5c9d20f76f99fdf55
st0o0 <64534642+st0o0@users.noreply.github.com>
2026-06-24 12:22:41 +0200

    chore(body): remove dead code from previous pump iterations

--- src/TurboHTTP/Protocol/Body/FlowControlledDrainSlot.cs
+++ src/TurboHTTP/Protocol/Body/FlowControlledDrainSlot.cs
@@ -5,31 +5,10 @@ namespace TurboHTTP.Protocol.Body;
 internal sealed class FlowControlledDrainSlot : BodyDrainSlot<int>, IResettable
 {
     public int ReservedWindow { get; set; }
-    public bool HasLimbo { get; private set; }
-    public ReadOnlyMemory<byte> LimboData { get; private set; }
-
-    public void StoreLimbo(ReadOnlyMemory<byte> data)
-    {
-        LimboData = data;
-        HasLimbo = true;
-    }
-
-    public void ShrinkLimbo(int consumed)
-    {
-        LimboData = LimboData[consumed..];
-    }
-
-    public void ClearLimbo()
-    {
-        LimboData = default;
-        HasLimbo = false;
-    }
 
     public override void Reset()
     {
         base.Reset();
         ReservedWindow = 0;
-        LimboData = default;
-        HasLimbo = false;
     }
 }
